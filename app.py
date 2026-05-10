48f2dffbb576e47bbfbf38e0ca18e9b974e91182
Toka Elfawey <tokaelfawey1@gmail.com>
2026-05-10 05:41:42 +0300

    app.py

--- app.py
+++ app.py
@@ -206,9 +206,9 @@ def main():
     # Footer المطور
     st.markdown("<br><hr>", unsafe_allow_html=True)
     st.markdown("""
-        <div style='text-align: center; color: #94a3b8; font-family: sans-serif; font-size: 14px;'>
+        <div style='text-align: center; color: #94a3b8; font-family: sans-serif; font-size: 18px;'>
             Developed with ❤️ by <b style='color: #00f5ff;'>Section 1 Team</b><br>
-            <span style='font-size: 12px;'>Toka Nasr | Aya Ahmed | Toka Alaa | Bavly Hany | Paula Moukhtar</span><br>
+            <span style='font-size: 12px;'>Toka Nasr | Aya Ahmed | Toka Alaa | Hemmat Hamdi | Nourhan Medhat</span><br>
             © 2026 Academic Project
         </div>
     """, unsafe_allow_html=True)
